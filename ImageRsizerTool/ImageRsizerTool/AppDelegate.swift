244ea29d4f1b11e5785bce4288ffc112a72c241f
Mostafizur Rahman <mostafizur.rahman@surecash.net>
2020-03-26 14:43:27 +0600

    project added

---
+++ ImageRsizerTool/ImageRsizerTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageRsizerTool
+//
+//  Created by Mostafizur Rahman on 26/3/20.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageRsizerTool/ImageRsizerTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageRsizerTool
+//
+//  Created by Mostafizur Rahman on 26/3/20.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
